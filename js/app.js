146f2ddb7468af198152fb1c9fe64ce7b2c7d1b3
mihatormoz <mcohotnik21@gmail.com>
2022-07-08 19:22:41 +0300

    Failed

--- js/app.js
+++ js/app.js
@@ -1,29 +1,41 @@
 document.querySelector('.input__send').addEventListener('click', () => {
     const title = document.querySelector('.input__task').value
-    // this.container.insertAdjacentHTML('beforeend', `
-    // <div class="active">
-    //     <h2 class="active__title">${title}</h2>
-    //     <p class="active__descr">${descr}</p>
-    //     <button class="active__completed">Completed</button>
-    //     <button class="active__ncomplited">Not completed</button>
-    // </div>
-    // `)
     if (title.length > 0) {
+        out()
+        let tit = []
+        tit.push(title)
+        console.log(tit)
+        localStorage.setItem('todo', JSON.stringify(tit))
+    }
+    function out() {
         const task = document.querySelector('.task__cont').insertAdjacentHTML('beforeend', `
-        <div class="active">
-            <h2 class="active__title">${title}</h2>
-            <label>
+            <div class="active">
+                <h2 class="active__title">${title}</h2>
                 <input type="checkbox" class="active__completed">
                 <p class="active__completed-text">Complited</p>
-            </label>
-        </div>
-    `)
+            </div>
+        `)
     }
-    document.querySelector('.active__completed').addEventListener('input', (event) => {
-        const checked = event.target
-        console.log(checked)
-        if (checked.checked) {
-            document.querySelector('.active').remove()
+    removeTodo()
+})
+if (localStorage.getItem('todo') != undefined) {
+    title = JSON.parse(localStorage.getItem('todo'))
+    const task = document.querySelector('.task__cont').insertAdjacentHTML('beforeend', `
+            <div class="active">
+                <h2 class="active__title">${title}</h2>
+                <input type="checkbox" class="active__completed">
+                <p class="active__completed-text">Complited</p>
+            </div>
+        `)
+    removeTodo()
+}
+function removeTodo() {
+    const checke = document.querySelector('.active')
+    checke.addEventListener('click', (e) => {
+        const chec = e.target
+        if (chec.checked) {
+            chec.parentElement.remove()
+            localStorage.removeItem('todo')
         }
     })
-})
+}
