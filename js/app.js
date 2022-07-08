5695c67b8cfc2763449bf2bc3e4506792b7074bd
mihatormoz <mcohotnik21@gmail.com>
2022-07-09 02:36:19 +0300

    Completed todo

--- js/app.js
+++ js/app.js
@@ -1,41 +1,65 @@
-document.querySelector('.input__send').addEventListener('click', () => {
-    const title = document.querySelector('.input__task').value
-    if (title.length > 0) {
-        out()
-        let tit = []
-        tit.push(title)
-        console.log(tit)
-        localStorage.setItem('todo', JSON.stringify(tit))
-    }
-    function out() {
-        const task = document.querySelector('.task__cont').insertAdjacentHTML('beforeend', `
-            <div class="active">
-                <h2 class="active__title">${title}</h2>
-                <input type="checkbox" class="active__completed">
-                <p class="active__completed-text">Complited</p>
-            </div>
-        `)
-    }
-    removeTodo()
-})
-if (localStorage.getItem('todo') != undefined) {
-    title = JSON.parse(localStorage.getItem('todo'))
-    const task = document.querySelector('.task__cont').insertAdjacentHTML('beforeend', `
-            <div class="active">
-                <h2 class="active__title">${title}</h2>
-                <input type="checkbox" class="active__completed">
-                <p class="active__completed-text">Complited</p>
-            </div>
-        `)
-    removeTodo()
+const taskInput = document.querySelector('.input__task')
+const taskBtn = document.querySelector('.input__send')
+const todosWrapper = document.querySelector('.task__cont')
+
+let tasks = []
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'))
+
+let todoArray = []
+
+function Task(description) {
+    this.description = description
+    this.completed = false
 }
-function removeTodo() {
-    const checke = document.querySelector('.active')
-    checke.addEventListener('click', (e) => {
-        const chec = e.target
-        if (chec.checked) {
-            chec.parentElement.remove()
-            localStorage.removeItem('todo')
-        }
-    })
+
+const createTask = (task, index) => {
+    return `
+        <div class="active ${task.completed ? 'checked' : ''}">
+            <h2 class="active__title">${task.description}</h2>
+            <div class="active-btn">
+                <input onclick="completeTask(${index})" type="checkbox" class="active__complete" ${task.completed ? 'checked' : ''}>
+                <button onclick="removeTask(${index})"  class="active__remove">Remove</button>
+            </div>
+        </div>
+    `
+}
+
+const fillHtmlList = () => {
+    todosWrapper.innerHTML = ""
+    if (tasks.length > 0) {
+        tasks.forEach((item, index) => {
+            todosWrapper.innerHTML += createTask(item, index)
+        })
+        todoArray = document.querySelectorAll('.active')
+    }
+}
+fillHtmlList()
+
+const updateLs = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+const completeTask = index => {
+    tasks[index].completed = !tasks[index].completed
+    if (tasks[index].completed) {
+        todoArray[index].classList.add('checked')
+    } else {
+        todoArray[index].classList.remove('checked')
+    }
+    updateLs()
+    fillHtmlList()
+}
+
+
+taskBtn.addEventListener('click', () => {
+    tasks.push(new Task(taskInput.value))
+    updateLs()
+    fillHtmlList()
+    taskInput.value = ''
+})
+
+const removeTask = index => {
+    tasks.splice(index, 1)
+    updateLs()
+    fillHtmlList()
 }
